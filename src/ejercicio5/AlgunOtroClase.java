4821f73a01697b198b1c4b5c313c6547d095f2d1
Admin <Admin@LAPTOP-O93FSHCL>
2024-04-30 09:07:58 +0200

    Creaccion de clases

---
+++ src/ejercicio5/AlgunOtroClase.java
@@ -0,0 +1,11 @@
+package ejercicio5;
+
+public class AlgunOtroClase {
+
+	public void setDireccion(Cliente cliente, String nuevaDireccion) {
+		cliente.setDireccion(nuevaDireccion);
+		// Otro código aquí
+	}
+}
+
+
